30a3eda883ae49edd2b79122df6c9be3c27070d9
mufaddal12 <mufaddal.d.786@gmail.com>
2020-04-07 21:39:40 +0530

    added exception class

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 Things left to do:
-1. Working on making objects
-2. Log file generation
-3. stdbuf commands??
-4. Add endgame exception
+1. process address space limit
--- newBM.py
+++ newBM.py
@@ -1,192 +1,3 @@
-"""
-from subprocess import call, Popen, PIPE
-from select import poll, POLLIN, POLLOUT
-from signal import signal, alarm, SIGALRM, SIGPIPE, SIG_DFL
-from sys import exit, argv
-from time import sleep
-
-
-# argv is a list of arguments passed along with the command to call this program
-# argv[0] : Name of Source file (here "newBm.py")
-# argv[1] : Extension of bot 1 : "c" or "cpp" or "py"
-# argv[2] : Extension of bot 2 : "c" or "cpp" or "py"
-# argv[3] : Name of file in which logs are to be written
-# argv[4] : If bot1 is playing as player 2 or not : "True" or "False"
-print(argv)
-(bot1Ext, bot2Ext, logFile, isP2) = (argv[1], argv[2], argv[3], argv[4])
-
-# botFile is where bots are kept
-botFile = "Bots/"
-# Time limit given to different types of bots
-time_limits = {
-    "cpp": 3000,
-    "c": 3000,
-    "py": 6000,
-}
-
-# Get the executable file of the validation code (in current directory)
-call("g++ -o val val.cpp".split())
-
-# Don't know the use of this as of now
-plist1 = ["prlimit", "-p", ""]
-plist2 = ["prlimit", "-p", ""]
-
-
-def logWrite(msg):
-    # Writing logs to logfile (name mentioned in argv[3])
-    print(msg)
-
-
-
-def processWrite(proc, string):
-    # Writing string of bytes to process proc
-    # str.encode(string) converts string to string of bytes
-    proc.stdin.write(str.encode(string))
-    proc.stdin.flush()
-
-
-def processRead(proc, poll=None):
-    # Reading output from given proc
-    ret = proc.stdout.readline()
-    ret = ret.decode().strip()
-    return ret
-
-
-def end_code(exit_code):
-    # Return code to system tellling how the game ended and also killing any open processes
-    # Program closes from here
-    if bot1.poll() is None:
-        bot1.kill()
-    if bot2.poll() is None:
-        bot2.kill()
-    if val.poll() is None:
-        val.kill()
-    exit(exit_code)
-
-
-def getArgList(exType, p):
-    # Get the command depending on type of extension and return a list of terminal commands
-    # example for running a python program, command is "python player1.py"
-    # but player1.py named bot is in directory botFile, so command is "python " + botFile + "player1"
-    # list is list of commands
-    lis = []
-    if exType == "cpp" or exType == "c":
-        lis = lis + ["./" + botFile + "player" + str(p)]
-        plist1.extend(("--nofile=5", "--nproc=500", "--as=21460"))
-    elif exType == "py":
-        lis = lis + ["python", botFile + "player" + str(p) + ".py"]
-        plist1.extend(("--nofile=5", "--nproc=500", "--as=32740"))
-    return lis
-
-
-# Popen is for opening a process by passing command line arguments
-# The input(stdin) and output(stdout) of these processes is changed to type PIPE
-# By default input and ouput would've been to the computer screen,
-# but with PIPE, the input and output are passed through a pipe to this process(parent)
-bot1 = Popen(args=getArgList(bot1Ext, 1), stdin=PIPE, stdout=PIPE)
-# poll classes is used to check if the process is ready to give output or not
-bot1Poll = poll()
-bot1Poll.register(bot1.stdout.fileno())
-bot1Time = time_limits[bot1Ext]
-
-bot2 = Popen(args=getArgList(bot2Ext, 2), stdin=PIPE, stdout=PIPE)
-bot2Poll = poll()
-bot2Poll.register(bot2.stdout.fileno())
-bot2Time = time_limits[bot2Ext]
-
-# The input and output from the bots will be passed through the validation process
-val = Popen(args=["./val"], stdin=PIPE, stdout=PIPE)
-
-# Dom't know this
-plist1[2] = str(bot1.pid)
-plist2[2] = str(bot2.pid)
-
-# call(plist1)  # dont know this
-# call(plist2)
-
-
-if isP2 == "True":
-    # If it was True in argv[4], then it means player 1 will play 2nd
-    # and so swapping the corresponding values
-    (bot1, bot2) = (bot2, bot1)
-    (bot1Poll, bot2Poll) = (bot2Poll, bot1Poll)
-    (bot1Time, bot2Time) = (bot2Time, bot1Time)
-
-# bot1 starts first hence passing 0
-msg = "0\n"
-processWrite(bot1, "0\n")
-
-# bot2 starts 2nd hence passing 1
-msg = "1\n"
-processWrite(bot2, "1\n")
-
-while True:
-    # poll().poll(time) waits for registered process to create an event(here give output to the PIPE)
-    # within given input time - time
-    # If an event is created wtihin time, then the output is read and temp is not empty
-    # If temp is not empty then poll().poll(time) return a list of tuples saying if output is ready or no
-    # so temp[0][1] is a POLLIN integer
-    if not bot1.poll():
-        temp = bot1Poll.poll(bot1Time)
-        if temp:
-            if temp[0][1] is POLLIN:
-                move = processRead(bot1)
-            else:
-                logWrite("v,0,No IO recieved")
-                end_code(102)
-        else:
-            logWrite("v,0,No IO recieved")
-            end_code(102)
-    else:
-        logWrite("v,0,PREMATURE TERMINATION")
-        end_code(102)
-
-    move = move + "\n"
-
-    processWrite(val, move)
-    validity = processRead(val)
-
-    # validity is the move if it was a valid move else it is INVALID
-
-    if validity.strip() != move.strip():
-        logWrite("v,0," + move.strip() + " " + validity.strip())
-        end_code(102)
-    print("v,0," + move.strip())
-
-    if not bot2.poll():
-        processWrite(bot2, move)
-        temp = bot2Poll.poll(bot2Time)
-        if temp:
-            if temp[0][1] is POLLIN:
-                move = processRead(bot2)
-            else:
-                logWrite("v,1,No IO recieved")
-                end_code(101)
-        else:
-            logWrite("v,1,No IO recieved")
-            end_code(101)
-
-    else:
-        logWrite("v,1,PREMATURE TERMINATION")
-        end_code(101)
-
-    move = move + "\n"
-
-    processWrite(val, move)
-    validity = processRead(val)
-
-    if validity.strip() != move.strip():
-        logWrite("v,1," + move.strip() + " " + validity.strip())
-        end_code(101)
-    print("v,1," + move.strip())
-
-    if not bot1.poll():
-        processWrite(bot1, move)
-
-end_code(101)
-"""
-
-
 # new approach to the code
 
 from subprocess import call, Popen, PIPE
@@ -424,3 +235,192 @@ while 1:
     logWrite("v,1," + move)
 
 a = input()
+
+
+"""
+from subprocess import call, Popen, PIPE
+from select import poll, POLLIN, POLLOUT
+from signal import signal, alarm, SIGALRM, SIGPIPE, SIG_DFL
+from sys import exit, argv
+from time import sleep
+
+
+# argv is a list of arguments passed along with the command to call this program
+# argv[0] : Name of Source file (here "newBm.py")
+# argv[1] : Extension of bot 1 : "c" or "cpp" or "py"
+# argv[2] : Extension of bot 2 : "c" or "cpp" or "py"
+# argv[3] : Name of file in which logs are to be written
+# argv[4] : If bot1 is playing as player 2 or not : "True" or "False"
+print(argv)
+(bot1Ext, bot2Ext, logFile, isP2) = (argv[1], argv[2], argv[3], argv[4])
+
+# botFile is where bots are kept
+botFile = "Bots/"
+# Time limit given to different types of bots
+time_limits = {
+    "cpp": 3000,
+    "c": 3000,
+    "py": 6000,
+}
+
+# Get the executable file of the validation code (in current directory)
+call("g++ -o val val.cpp".split())
+
+# Don't know the use of this as of now
+plist1 = ["prlimit", "-p", ""]
+plist2 = ["prlimit", "-p", ""]
+
+
+def logWrite(msg):
+    # Writing logs to logfile (name mentioned in argv[3])
+    print(msg)
+
+
+
+def processWrite(proc, string):
+    # Writing string of bytes to process proc
+    # str.encode(string) converts string to string of bytes
+    proc.stdin.write(str.encode(string))
+    proc.stdin.flush()
+
+
+def processRead(proc, poll=None):
+    # Reading output from given proc
+    ret = proc.stdout.readline()
+    ret = ret.decode().strip()
+    return ret
+
+
+def end_code(exit_code):
+    # Return code to system tellling how the game ended and also killing any open processes
+    # Program closes from here
+    if bot1.poll() is None:
+        bot1.kill()
+    if bot2.poll() is None:
+        bot2.kill()
+    if val.poll() is None:
+        val.kill()
+    exit(exit_code)
+
+
+def getArgList(exType, p):
+    # Get the command depending on type of extension and return a list of terminal commands
+    # example for running a python program, command is "python player1.py"
+    # but player1.py named bot is in directory botFile, so command is "python " + botFile + "player1"
+    # list is list of commands
+    lis = []
+    if exType == "cpp" or exType == "c":
+        lis = lis + ["./" + botFile + "player" + str(p)]
+        plist1.extend(("--nofile=5", "--nproc=500", "--as=21460"))
+    elif exType == "py":
+        lis = lis + ["python", botFile + "player" + str(p) + ".py"]
+        plist1.extend(("--nofile=5", "--nproc=500", "--as=32740"))
+    return lis
+
+
+# Popen is for opening a process by passing command line arguments
+# The input(stdin) and output(stdout) of these processes is changed to type PIPE
+# By default input and ouput would've been to the computer screen,
+# but with PIPE, the input and output are passed through a pipe to this process(parent)
+bot1 = Popen(args=getArgList(bot1Ext, 1), stdin=PIPE, stdout=PIPE)
+# poll classes is used to check if the process is ready to give output or not
+bot1Poll = poll()
+bot1Poll.register(bot1.stdout.fileno())
+bot1Time = time_limits[bot1Ext]
+
+bot2 = Popen(args=getArgList(bot2Ext, 2), stdin=PIPE, stdout=PIPE)
+bot2Poll = poll()
+bot2Poll.register(bot2.stdout.fileno())
+bot2Time = time_limits[bot2Ext]
+
+# The input and output from the bots will be passed through the validation process
+val = Popen(args=["./val"], stdin=PIPE, stdout=PIPE)
+
+# Dom't know this
+plist1[2] = str(bot1.pid)
+plist2[2] = str(bot2.pid)
+
+# call(plist1)  # dont know this
+# call(plist2)
+
+
+if isP2 == "True":
+    # If it was True in argv[4], then it means player 1 will play 2nd
+    # and so swapping the corresponding values
+    (bot1, bot2) = (bot2, bot1)
+    (bot1Poll, bot2Poll) = (bot2Poll, bot1Poll)
+    (bot1Time, bot2Time) = (bot2Time, bot1Time)
+
+# bot1 starts first hence passing 0
+msg = "0\n"
+processWrite(bot1, "0\n")
+
+# bot2 starts 2nd hence passing 1
+msg = "1\n"
+processWrite(bot2, "1\n")
+
+while True:
+    # poll().poll(time) waits for registered process to create an event(here give output to the PIPE)
+    # within given input time - time
+    # If an event is created wtihin time, then the output is read and temp is not empty
+    # If temp is not empty then poll().poll(time) return a list of tuples saying if output is ready or no
+    # so temp[0][1] is a POLLIN integer
+    if not bot1.poll():
+        temp = bot1Poll.poll(bot1Time)
+        if temp:
+            if temp[0][1] is POLLIN:
+                move = processRead(bot1)
+            else:
+                logWrite("v,0,No IO recieved")
+                end_code(102)
+        else:
+            logWrite("v,0,No IO recieved")
+            end_code(102)
+    else:
+        logWrite("v,0,PREMATURE TERMINATION")
+        end_code(102)
+
+    move = move + "\n"
+
+    processWrite(val, move)
+    validity = processRead(val)
+
+    # validity is the move if it was a valid move else it is INVALID
+
+    if validity.strip() != move.strip():
+        logWrite("v,0," + move.strip() + " " + validity.strip())
+        end_code(102)
+    print("v,0," + move.strip())
+
+    if not bot2.poll():
+        processWrite(bot2, move)
+        temp = bot2Poll.poll(bot2Time)
+        if temp:
+            if temp[0][1] is POLLIN:
+                move = processRead(bot2)
+            else:
+                logWrite("v,1,No IO recieved")
+                end_code(101)
+        else:
+            logWrite("v,1,No IO recieved")
+            end_code(101)
+
+    else:
+        logWrite("v,1,PREMATURE TERMINATION")
+        end_code(101)
+
+    move = move + "\n"
+
+    processWrite(val, move)
+    validity = processRead(val)
+
+    if validity.strip() != move.strip():
+        logWrite("v,1," + move.strip() + " " + validity.strip())
+        end_code(101)
+    print("v,1," + move.strip())
+
+    if not bot1.poll():
+        processWrite(bot1, move)
+
+end_code(101)
+"""
